28c5f27e77c025bd401277c594043acfe1f0c6cb
Rallve <lucas.liedgren@gmail.com>
2023-02-07 14:58:34 +0100

    gjorde saker

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,35 @@
 const express = require('express');
 const router = express.Router();
+const promisePool = require('../utils/database.js');
+const bcrypt = require('bcrypt');
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('index.njk', { title: 'Express' });
+    res.render('index.njk', { title: 'Login ALC' });
+});
+
+router.get('/login', function(req, res, next) {
+    res.render('form.njk', { title: 'Login ALC' });
+});
+
+router.post('/login', async function(req, res, next) { 
+    const { username, password} = req.body;
+    const encrypted = password.hash; 
+    console.log(encrypted);
+    const [rows] = await promisePool.query("INSERT INTO unusers (name, password) VALUES (?,?)", [username, encrypted]);
+
+});
+
+//Does this make sense???
+router.get('/:pwd', function(req, res, next) {
+    const pwd = req.body.pwd;
+    res.render('form.njk', { title: 'Login ALC' });
+});
+bcrypt.hash(LÖSENORDSSTRÄNG, 10, function (err, hash) {
+
+    console.log(hash);
+    return res.json(hash);
 });
 
 module.exports = router;
